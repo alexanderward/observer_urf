18597ded6cf27e9e58de27fb38693436ea0a43ad
Alexander Ward <alexander.ward1@gmail.com>
2019-07-28 18:45:42 -0500

    changed db

--- webapp/backend/service/settings/prod.py
+++ webapp/backend/service/settings/prod.py
@@ -6,7 +6,7 @@ DATABASES = {
         'NAME': "urf",
         'USER': "admin",
         'PASSWORD': "alex3412",
-        'HOST': "urf.cluster-chdthce697hh.us-east-1.rds.amazonaws.com",
+        'HOST': "urf.chdthce697hh.us-east-1.rds.amazonaws.com",
         'PORT': 3306,
         'ATOMIC_REQUESTS': True,
         'OPTIONS': {
